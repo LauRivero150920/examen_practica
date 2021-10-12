b13bbe5e6c4cf8744b806da9f6c562cced5978b1
LauRivero150920 <A01274144@itesm.mx>
2021-10-11 20:12:39 -0500

    Cleanup de archivos base

--- routes/park_incidents-routes.js
+++ routes/park_incidents-routes.js
@@ -4,19 +4,10 @@ const incidentsController = require('../controllers/park_incidents-controller');
 
 const router = express.Router();
 
-// De lo particular a lo general 
 router.get('/add', incidentsController.addIncident);
 
 router.get('/list', incidentsController.getIncident);
 
 router.post('/add', incidentsController.postIncident);
-
-/*
-router.get('/logout', incidentsController.getLogout);
-
-router.get('/add', incidentsController.getAdd);
-
-router.post('/add', incidentsController.postAdd);
-*/
 
 module.exports = router;
